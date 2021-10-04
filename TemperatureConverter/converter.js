7dbe7e788589828e4da4c57356eedcb9c65499c3
Siddartha Mareedu <mareedusiddartha@gmail.com>
2021-10-04 08:03:16 +0530

    Fifth Commit

--- TemperatureConverter/converter.js
+++ TemperatureConverter/converter.js
@@ -2,7 +2,7 @@ const calculateTemp = () => {
   const numberTemp = document.getElementById("temp").value;
 
   const tempSelected = document.getElementById("temp_diff");
-  const valueTemp = temp_diff.options[tempSelected.SelectedIndex].value;
+  const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
 
   const celToFah = (cel) => {
     let fahrenheit = Math.round((cel * 9) / 5 + 32);
